c19b730406e45990efae9b8dd2fae12281c57271
Prasad Lakhpati <prasadlakhpati102@gmail.com>
2025-10-06 14:32:52 +0530

    add user avatar and cover image update functionality with cloud upload

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -274,6 +274,64 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, user, "Account details updated successfully"))
 })
+
+const updateUserAvatar = asyncHandler( async (req, res) => {
+
+    const avatarLocalPath = req.file?.avatar
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400, "Error while uploading avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+           $set:{
+            avatar: avatar.url
+           }
+        },
+        { new: true }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(200, user, "avatar image updated successfully")
+})
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+    
+    const coverImageLocalPath = req.file?.coverImage
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "Cover Image file is missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImage.url){
+        throw new ApiError(400, "Error while uploading cover image")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage: coverImage.url
+            }
+        },
+        { new: true }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(200, user, "cover image updated successfully")
+})
  
 export {
     registerUser,
@@ -283,4 +341,6 @@ export {
     changeCurrentPassword,
     getCurrentUser,
     updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
 }
