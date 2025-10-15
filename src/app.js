9734df003b264ae1983e4e0971a9d2cfcff48b50
Prasad Lakhpati <prasadlakhpati102@gmail.com>
2025-10-15 19:16:59 +0530

    feat: add new routes and controllers for comments, likes, playlists, subscriptions, tweets, videos, and healthcheck

--- src/app.js
+++ src/app.js
@@ -31,6 +31,14 @@ app.use(cookieParser())
 
 //routes import
 import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes decleration
 app.use("/api/v1/users", userRouter) // whenever any user types /users it will give control to userRouter, then userRouter do further. ***Here we are not using app.get() where we write routes and controller at one place but we are using middleware app.use() method as our routes and middlewares are defined differently 
@@ -39,5 +47,14 @@ app.use("/api/v1/users", userRouter) // whenever any user types /users it will g
 //now automatically the url will be http://localhost:8000/api/v1/users/login
 //therefore, we have not to import anything in this file(app.js)
 //only app.use("/api/v1/users", userRouter) is sufficient because whenever user type /api/v1/users the control is sent to userRouter 
+
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 export { app }
